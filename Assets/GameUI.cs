b9c3004c827ad8adbce92f5869332cce0399989c
Jiyan kumek <jiyankumek@gmail.com>
2025-03-23 17:43:24 +0300

    Equipped

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -18,45 +18,67 @@ public class GameUI : MonoBehaviour
     public GameObject nightStalkertSight;
 
     public GameObject equipButton;
-    public TMP_Text selectedSightButtonText;
+    private Button equipbuttonn;
+    public TMP_Text equipButtonText;
 
-    private Button selectedSightButton = null;
-    
+    private int currentEquippedSight = 0; // Þu anda donatýlmýþ sight (0: default, 1: thermal, 2: nightStalker)
+    private int selectedSightIndex = -1; // Þu anda seçili sight (-1: hiçbiri seçili deðil)
 
     private Button selectedButtonAttachment = null;
     private Button selectedButtonSight = null;
 
-
-    
-    public void OnClickEquip(Button button)
+    private void Start()
     {
-        if (selectedSightButton.interactable == true)
-        {
-            button.interactable = true;
-            selectedSightButtonText.text = "Equýp";
-        }
-        button.interactable = false;
-        selectedSightButtonText.text = "Equýpped";
+        equipbuttonn = equipButton.GetComponent<Button>();
+        LoadSelectedSight(); // Kaydedilen sight'ý yükle
+        UpdateEquipButton(); // Buton durumunu güncelle
     }
-    private void SelectSight(Button button)
+
+    public void OnClickEquip()
     {
-        // Yeni seçili butonu güncelle
-        selectedSightButton = button;
-        selectedSightButton.interactable = true;
+        //if (selectedSightIndex == -1) return;
+
+        currentEquippedSight = selectedSightIndex;
+       
+
+        UpdateSightOnWeapon(); // Silahý modifiye et
+        equipButtonText.text = "Equipped";
+        equipbuttonn.interactable = false;
+        PlayerPrefs.SetInt("SelectedSightIndex", currentEquippedSight);
+        PlayerPrefs.Save();
+        Debug.Log("Donatýlan Sight: " + currentEquippedSight);
+    }
+    private void UpdateSightOnWeapon()
+    {
+        defaultSight.SetActive(false);
+        thermalSight.SetActive(false);
+        nightStalkertSight.SetActive(false);
+
+        if (currentEquippedSight == 0)
+        {
+            defaultSight.SetActive(true);
+        }
+        else if (currentEquippedSight == 1)
+        {
+            thermalSight.SetActive(true);
+        }
+        else if (currentEquippedSight == 2)
+        {
+            nightStalkertSight.SetActive(true);
+        }
     }
     public void SelectButtonAttachment(Button button)
     {
-        
         if (selectedButtonAttachment != null)
         {
             selectedButtonAttachment.interactable = true;
         }
         selectedButtonAttachment = button;
-        selectedButtonAttachment.interactable = false; 
+        selectedButtonAttachment.interactable = false;
     }
+
     public void SelectButtonSight(Button button)
     {
-
         if (selectedButtonSight != null)
         {
             selectedButtonSight.interactable = true;
@@ -68,23 +90,23 @@ public class GameUI : MonoBehaviour
     public void OnClickAttachmentsButton()
     {
         attachmentsButton.SetActive(false);
-        attachments.anchoredPosition =new Vector2(0,140);
+        attachments.anchoredPosition = new Vector2(0, 140);
 
-        for(int i = 0; i < attachmentsNametext.Length; i++)
+        for (int i = 0; i < attachmentsNametext.Length; i++)
         {
-            attachmentsNametext[i].gameObject.SetActive(true); 
+            attachmentsNametext[i].gameObject.SetActive(true);
         }
     }
 
     public void OnClickGrid_1()
     {
         OnClickAttachmentsButton();
-        sightScrollView.SetActive(true);    
+        sightScrollView.SetActive(true);
     }
+
     public void OnClickGrid_2()
     {
         OnClickAttachmentsButton();
-        //SightScrollView.SetActive(true);
     }
 
     public void OnClickSight_1(Button button)
@@ -92,25 +114,58 @@ public class GameUI : MonoBehaviour
         defaultSight.SetActive(true);
         thermalSight.SetActive(false);
         nightStalkertSight.SetActive(false);
-        equipButton.SetActive(true);
-        SelectSight(button);
-        
+        selectedSightIndex = 0;
+        UpdateEquipButton();
     }
+
     public void OnClickSight_2(Button button)
     {
         defaultSight.SetActive(false);
         thermalSight.SetActive(true);
         nightStalkertSight.SetActive(false);
-        equipButton.SetActive(true);
-        SelectSight(button);
+        selectedSightIndex = 1;
+        UpdateEquipButton();
     }
+
     public void OnClickSight_3(Button button)
     {
         defaultSight.SetActive(false);
         thermalSight.SetActive(false);
         nightStalkertSight.SetActive(true);
-        equipButton.SetActive(true);
-        SelectSight(button);
+        selectedSightIndex = 2;
+        UpdateEquipButton();
     }
 
+    private void UpdateEquipButton()
+    {
+        equipButton.SetActive(true);
+        if (selectedSightIndex == currentEquippedSight)
+        {
+            equipButtonText.text = "Equipped";
+            equipbuttonn.interactable = false;
+        }
+        else
+        {
+            equipButtonText.text = "Equip";
+            equipbuttonn.interactable = true;
+        }
+    }
+
+    private void LoadSelectedSight()
+    {
+        if (PlayerPrefs.HasKey("SelectedSightIndex"))
+        {
+            currentEquippedSight = PlayerPrefs.GetInt("SelectedSightIndex");
+        }
+        else
+        {
+            currentEquippedSight = 0;
+        }
+        selectedSightIndex = currentEquippedSight;
+        UpdateSightOnWeapon();
+
+        Debug.Log("Yüklenen Sight: " + currentEquippedSight);
+    }
+
+    
 }
