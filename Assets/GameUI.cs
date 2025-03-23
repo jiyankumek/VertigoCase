a4455f0212724e653852b680dcfb5e6d17ba7df7
Jiyan kumek <jiyankumek@gmail.com>
2025-03-23 19:14:52 +0300

    UI_Bitti_gibi

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -12,17 +12,27 @@ public class GameUI : MonoBehaviour
 
     public GameObject selectImage;
     public GameObject sightScrollView;
+    public GameObject barrelScroolView;
 
     public GameObject defaultSight;
     public GameObject thermalSight;
     public GameObject nightStalkertSight;
 
+    public GameObject defaultBarrel;
+    public GameObject coolBreeze;
+    public GameObject peaceKeeper;
+
     public GameObject equipButton;
     private Button equipbuttonn;
     public TMP_Text equipButtonText;
 
+    // Sight için deðiþkenler
     private int currentEquippedSight = 0; // Þu anda donatýlmýþ sight (0: default, 1: thermal, 2: nightStalker)
-    private int selectedSightIndex = -1; // Þu anda seçili sight (-1: hiçbiri seçili deðil)
+    private int selectedSightIndex = -1;  // hiçbiri seçili deðil
+
+    // Barrel için deðiþkenler
+    private int currentEquippedBarrel = 0; // Þu anda donatýlmýþ barrel (0: default, 1: coolBreeze, 2: peaceKeeper)
+    private int selectedBarrelIndex = -1;  // hiçbiri seçili deðil
 
     private Button selectedButtonAttachment = null;
     private Button selectedButtonSight = null;
@@ -30,42 +40,84 @@ public class GameUI : MonoBehaviour
     private void Start()
     {
         equipbuttonn = equipButton.GetComponent<Button>();
-        LoadSelectedSight(); // Kaydedilen sight'ý yükle
-        UpdateEquipButton(); // Buton durumunu güncelle
+        LoadSelectedSight(); 
+        UpdateEquipButton(); 
     }
 
     public void OnClickEquip()
     {
-        //if (selectedSightIndex == -1) return;
+        // Sight
+        if (selectedSightIndex != -1)
+        {
+            currentEquippedSight = selectedSightIndex;
+            UpdateSightOnWeapon();
+            PlayerPrefs.SetInt("SelectedSightIndex", currentEquippedSight);
+        }
 
-        currentEquippedSight = selectedSightIndex;
-       
+        // Barrel
+        if (selectedBarrelIndex != -1)
+        {
+            currentEquippedBarrel = selectedBarrelIndex;
+            UpdateBarrelOnWeapon();
+            PlayerPrefs.SetInt("SelectedBarrelIndex", currentEquippedBarrel);
+        }
 
-        UpdateSightOnWeapon(); // Silahý modifiye et
+        PlayerPrefs.Save();
         equipButtonText.text = "Equýpped";
         equipbuttonn.interactable = false;
-        PlayerPrefs.SetInt("SelectedSightIndex", currentEquippedSight);
-        PlayerPrefs.Save();
+
         Debug.Log("Donatýlan Sight: " + currentEquippedSight);
+        Debug.Log("Donatýlan Barrel: " + currentEquippedBarrel);
     }
+
     private void UpdateSightOnWeapon()
     {
         defaultSight.SetActive(false);
         thermalSight.SetActive(false);
         nightStalkertSight.SetActive(false);
 
-        if (currentEquippedSight == 0)
+        if (currentEquippedSight == 0) defaultSight.SetActive(true);
+        else if (currentEquippedSight == 1) thermalSight.SetActive(true);
+        else if (currentEquippedSight == 2) nightStalkertSight.SetActive(true);
+    }
+    private void UpdateBarrelOnWeapon()
+    {
+        defaultBarrel.SetActive(false);
+        coolBreeze.SetActive(false);
+        peaceKeeper.SetActive(false);
+
+        if (currentEquippedBarrel == 0) defaultBarrel.SetActive(true);
+        else if (currentEquippedBarrel == 1) coolBreeze.SetActive(true);
+        else if (currentEquippedBarrel == 2) peaceKeeper.SetActive(true);
+    }
+    private void LoadSelectedAttachments()
+    {
+        // Sight'ý yükle
+        if (PlayerPrefs.HasKey("SelectedSightIndex"))
         {
-            defaultSight.SetActive(true);
+            currentEquippedSight = PlayerPrefs.GetInt("SelectedSightIndex");
         }
-        else if (currentEquippedSight == 1)
+        else
         {
-            thermalSight.SetActive(true);
+            currentEquippedSight = 0;
         }
-        else if (currentEquippedSight == 2)
+        selectedSightIndex = currentEquippedSight;
+        UpdateSightOnWeapon();
+
+        // Barrel'ý yükle
+        if (PlayerPrefs.HasKey("SelectedBarrelIndex"))
         {
-            nightStalkertSight.SetActive(true);
+            currentEquippedBarrel = PlayerPrefs.GetInt("SelectedBarrelIndex");
         }
+        else
+        {
+            currentEquippedBarrel = 0;
+        }
+        selectedBarrelIndex = currentEquippedBarrel;
+        UpdateBarrelOnWeapon();
+
+        Debug.Log("Yüklenen Sight: " + currentEquippedSight);
+        Debug.Log("Yüklenen Barrel: " + currentEquippedBarrel);
     }
     public void SelectButtonAttachment(Button button)
     {
@@ -98,55 +150,91 @@ public class GameUI : MonoBehaviour
         }
     }
 
-    public void OnClickGrid_1()
+    public void OnClickGrid_1()//sight
     {
         OnClickAttachmentsButton();
         sightScrollView.SetActive(true);
+        barrelScroolView.SetActive(false);
     }
 
-    public void OnClickGrid_2()
+    public void OnClickGrid_3()//barrel
     {
         OnClickAttachmentsButton();
+        sightScrollView.SetActive(false);
+        barrelScroolView.SetActive(true);
     }
-
     public void OnClickSight_1(Button button)
     {
+        selectedSightIndex = 0;
+        
         defaultSight.SetActive(true);
         thermalSight.SetActive(false);
         nightStalkertSight.SetActive(false);
-        selectedSightIndex = 0;
         UpdateEquipButton();
     }
 
     public void OnClickSight_2(Button button)
     {
+        selectedSightIndex = 1;
+        
         defaultSight.SetActive(false);
         thermalSight.SetActive(true);
         nightStalkertSight.SetActive(false);
-        selectedSightIndex = 1;
         UpdateEquipButton();
     }
 
     public void OnClickSight_3(Button button)
     {
+        selectedSightIndex = 2;
+        
         defaultSight.SetActive(false);
         thermalSight.SetActive(false);
         nightStalkertSight.SetActive(true);
-        selectedSightIndex = 2;
+        UpdateEquipButton();
+    }
+
+    public void OnClickBarrel_1(Button button)
+    {
+        selectedBarrelIndex = 0;
+        
+        defaultBarrel.SetActive(true);
+        coolBreeze.SetActive(false);
+        peaceKeeper.SetActive(false);
+        UpdateEquipButton();
+    }
+
+    public void OnClickBarrel_2(Button button)
+    {
+        selectedBarrelIndex = 1;
+        
+        defaultBarrel.SetActive(false);
+        coolBreeze.SetActive(true);
+        peaceKeeper.SetActive(false);
+        UpdateEquipButton();
+    }
+
+    public void OnClickBarrel_3(Button button)
+    {
+        selectedBarrelIndex = 2;
+        
+        defaultBarrel.SetActive(false);
+        coolBreeze.SetActive(false);
+        peaceKeeper.SetActive(true);
         UpdateEquipButton();
     }
 
     private void UpdateEquipButton()
     {
         equipButton.SetActive(true);
-        if (selectedSightIndex == currentEquippedSight)
+        // Hem sight hem de barrel için kontrol et
+        if (selectedSightIndex == currentEquippedSight && selectedBarrelIndex == currentEquippedBarrel)
         {
-            equipButtonText.text = "Equýpped";
+            equipButtonText.text = "Equýpped"; 
             equipbuttonn.interactable = false;
         }
         else
         {
-            equipButtonText.text = "Equýp";
+            equipButtonText.text = "Equýp"; 
             equipbuttonn.interactable = true;
         }
     }
